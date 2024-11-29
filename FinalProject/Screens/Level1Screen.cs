856d8545e2a888496c28292049ff418ea509f203
EthsMC <ethankeating1313@gmail.com>
2024-11-29 17:11:25 -0500

    Added scrolling background to level 1

--- FinalProject/Screens/Level1Screen.cs
+++ FinalProject/Screens/Level1Screen.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,22 +12,29 @@ namespace FinalProject.Screens
 {
     class Level1Screen : IScreen
     {
-        public ScreenType ScreenType => ScreenType.Menu;
+        public ScreenType ScreenType => ScreenType.Level1;
         private Game _game;
+
+        Texture2D backgroundSprite;
+
+        public Vector2 backgroundPosition = new Vector2(0, 0);
 
         public Level1Screen(Game game)
         {
             _game = game;
+
+            backgroundSprite = _game.Content.Load<Texture2D>("images/background");
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
+            spriteBatch.Draw(backgroundSprite, backgroundPosition, Color.White);
             //spriteBatch.Draw(null, null, null);
         }
 
         public void Update(float delta)
         {
+            backgroundPosition.X -= delta * 20; //Temporary scroll speed (20 units per second)
         }
     }
 }
