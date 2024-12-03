08a5e114bbd2d7850a1652cbc0ef2e8514a48dd9
EthsMC <ethankeating1313@gmail.com>
2024-12-03 14:35:14 -0500

    Updated pearl

--- FinalProject/Screens/Level1Screen.cs
+++ FinalProject/Screens/Level1Screen.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using FinalProject.Entities;
+using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
@@ -51,15 +52,15 @@ namespace FinalProject.Screens
             enemies = [shark1, shark2];
             sharks = [shark1, shark2];
 
-            pearl1 = new Pearl(_game, spriteBatch, new Vector2(100, 100));
+            pearl1 = new Pearl(_game, spriteBatch, new Vector2(100, 500));
             pearl1.Initialize();
-            pearl2 = new Pearl(_game, spriteBatch, new Vector2(200, 100));
+            pearl2 = new Pearl(_game, spriteBatch, new Vector2(200, 500));
             pearl2.Initialize();
-            pearl3 = new Pearl(_game, spriteBatch, new Vector2(300, 100));
+            pearl3 = new Pearl(_game, spriteBatch, new Vector2(300, 500));
             pearl3.Initialize();
-            pearl4 = new Pearl(_game, spriteBatch, new Vector2(400, 100));
+            pearl4 = new Pearl(_game, spriteBatch, new Vector2(400, 500));
             pearl4.Initialize();
-            pearl5 = new Pearl(_game, spriteBatch, new Vector2(500, 100));
+            pearl5 = new Pearl(_game, spriteBatch, new Vector2(500, 500));
             pearl5.Initialize();
 
             pearls = [pearl1, pearl2, pearl3, pearl4, pearl5];
@@ -81,10 +82,7 @@ namespace FinalProject.Screens
 
             foreach (Pearl pearl in pearls)
             {
-                if (!pearl.IsCollected)
-                {
-                    pearl.Draw();
-                }
+                pearl.Draw();
             }
         }
 
@@ -109,10 +107,15 @@ namespace FinalProject.Screens
                 {
                     player.Position = new Vector2(startX, player.Position.Y);
                     backgroundPosition.X -= deltaX;
-                    
+
                     foreach (Shark shark in sharks)
                     {
                         shark.UpdateBounds(deltaX);
+                    }
+
+                    foreach (Pearl pearl in pearls)
+                    {
+                        pearl.Position = new Vector2(pearl.Position.X - deltaX, pearl.Position.Y);
                     }
                 }
             }
@@ -138,6 +141,10 @@ namespace FinalProject.Screens
                     foreach (Shark shark in sharks)
                     {
                         shark.UpdateBounds(deltaX);
+                    }
+                    foreach (Pearl pearl in pearls)
+                    {
+                        pearl.Position = new Vector2(pearl.Position.X - deltaX, pearl.Position.Y);
                     }
                 }
             }
@@ -187,6 +194,11 @@ namespace FinalProject.Screens
             foreach (Shark shark in sharks)
             {
                 shark.ResetBounds();
+            }
+
+            foreach(Pearl pearl in pearls)
+            {
+                pearl.Position = pearl.StartingPosition;
             }
         }
     }
