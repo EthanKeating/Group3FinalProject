210e0c60bdd4d8f204a3c6b9dd6cf6b84907e592
MVanRaay <martinvraay@gmail.com>
2024-12-03 14:00:55 -0500

    added pearl class

---
+++ FinalProject/Entities/Crab.cs
@@ -0,0 +1,28 @@
+﻿namespace FinalProject.Entities
+{
+    public class Crab : Enemy
+    {
+        public Crab(Game game, Vector2 position, int speed) : base(game, position, speed)
+        {
+            Texture = game.Content.Load<Texture2D>("images/idleEvilCrab");
+            Width = Texture.Width / 2;
+            Height = Texture.Height;
+        }
+
+        public void Initialize()
+        {
+            Hitbox = new Hitbox(this, 15, 80, 60, 0);
+            AttackHitbox = new Hitbox(this, 20, 80, 80, 0);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            
+        }
+
+        public override void Move()
+        {
+            
+        }
+    }
+}
